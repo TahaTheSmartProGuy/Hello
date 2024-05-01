0a9f0499c3bce470a2a61e91109a2cca39a3b735
Taha! <164132872+TahaTheSmartProGuy@users.noreply.github.com>
2024-05-01 09:53:53 +0300

    Add files via upload

--- Hello1.5_Safety.bat
+++ Hello1.5_Safety.bat
@@ -1,6 +1,6 @@
 @Echo off 
 Title Warning!
-Echo This Malware Doesnt Disable TaskMgr Do You Want To Run it?
+Echo This Malware Doesnt Disable TaskMgr And Reg Do You Want To Run it?
 Pause
 Echo Your PC Has Been Not infected By Hello1.5_Safety By Taha 2.0.2.4 >"C:\Note.txt"
 Start C:\Note.txt
